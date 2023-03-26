afa36aa49062f878877cb60376079c8c3379956c
jaseel-koolath <112375897+jaseel-koolath@users.noreply.github.com>
2023-03-26 15:04:04 +0530

    Lesson 3

--- Lesson1/build/js/main.js
+++ Lesson1/build/js/main.js
@@ -27,5 +27,62 @@ let isActive = true;
 let regExp = /abc/;
 // create an example array type
 let myArray = [1, 2, 3];
-// create an example tuple type
-let myTuple = [1, '2'];
+let stringArr = ['a', 'b', 'c'];
+let numberArr = [1, 2, 3, 'jaseel'];
+let booleanArr = [true, false, true];
+stringArr.push(2);
+// this will throw an error because the array is a string array
+stringArr[0] = 2;
+numberArr[0] = 'me';
+// this is fine because the array is a number or string array
+stringArr = numberArr;
+// this will throw an error because the array is a string array
+numberArr = stringArr;
+// this is fine because the array is a number or string array
+// create an emepty array
+let test = [];
+let band = [];
+let myTuple = [1, '2', true];
+let mixed = ['hello', 1, true];
+mixed = myTuple; // this is fine because mixed can accept in any order
+myTuple = mixed; // this is not fine because myTuple is a tuple and must be in order
+//object
+let myObj = {
+    name: 'Jaseel',
+    age: 24,
+};
+myObj = mixed;
+let jaseel = {
+    name: 'Jaseel',
+    // age: 24,
+    email: '',
+    address: {
+        city: 'San Francisco',
+        state: 'CA',
+    },
+    albums: ['The Wall', 1],
+};
+const printUser = (user) => {
+    var _a, _b;
+    console.log((_a = user.name) === null || _a === void 0 ? void 0 : _a.toUpperCase());
+    console.log(user === null || user === void 0 ? void 0 : user.age);
+    console.log(user.email);
+    console.log(user.address.city);
+    console.log(user.address.state);
+    console.log(user.albums);
+    return (_b = user.name) === null || _b === void 0 ? void 0 : _b.toUpperCase();
+};
+//Enums
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 1] = "U";
+    Grade[Grade["D"] = 2] = "D";
+    Grade[Grade["C"] = 3] = "C";
+    Grade[Grade["B"] = 4] = "B";
+    Grade[Grade["A"] = 5] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade.U);
+console.log(Grade.D);
+console.log(Grade.C);
+console.log(Grade.B);
+console.log(Grade.A);
--- Lesson1/src/main.ts
+++ Lesson1/src/main.ts
@@ -27,5 +27,92 @@ let isActive: boolean | number = true
 let regExp: RegExp = /abc/
 // create an example array type
 let myArray: number[] = [1, 2, 3]
-// create an example tuple type
-let myTuple: [number, string] = [1, '2']
+
+let stringArr = ['a', 'b', 'c']
+let numberArr = [1, 2, 3, 'jaseel']
+let booleanArr = [true, false, true]
+
+stringArr.push(2)
+// this will throw an error because the array is a string array
+stringArr[0] = 2
+
+numberArr[0] = 'me'
+// this is fine because the array is a number or string array
+
+stringArr = numberArr
+// this will throw an error because the array is a string array
+numberArr = stringArr
+// this is fine because the array is a number or string array
+
+// create an emepty array
+let test = []
+let band: string[] = []
+let myTuple: [number, string, boolean] = [1, '2', true]
+let mixed = ['hello', 1, true]
+
+mixed = myTuple // this is fine because mixed can accept in any order
+myTuple = mixed // this is not fine because myTuple is a tuple and must be in order
+
+//object
+let myObj = {
+  name: 'Jaseel',
+  age: 24,
+}
+myObj = mixed
+
+type userData = {
+  name?: string
+  age?: number
+  email: string
+  address: {
+    city: string
+    state: string
+  }
+  albums: (string | number)[]
+}
+
+// instead of a type we can also use an interface
+interface userData2 {
+  name: string
+  age?: number
+  email: string
+  address: {
+    city: string
+    state: string
+  }
+  albums: (string | number)[]
+}
+
+let jaseel: userData = {
+  name: 'Jaseel',
+  // age: 24,
+  email: '',
+  address: {
+    city: 'San Francisco',
+    state: 'CA',
+  },
+  albums: ['The Wall', 1],
+}
+const printUser = (user: userData) => {
+  console.log(user.name?.toUpperCase())
+  console.log(user?.age)
+  console.log(user.email)
+  console.log(user.address.city)
+  console.log(user.address.state)
+  console.log(user.albums)
+  return user.name?.toUpperCase()
+}
+
+//Enums
+enum Grade {
+  U = 1,
+  D,
+  C,
+  B,
+  A,
+}
+console.log(Grade.U)
+console.log(Grade.D)
+console.log(Grade.C)
+console.log(Grade.B)
+console.log(Grade.A)
